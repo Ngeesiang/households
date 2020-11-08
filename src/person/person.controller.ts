bb5d86637b9633ef1e4b8756542930738f01171c
Ngee Siang <nathan.lingns@gmail.com>
2020-11-08 19:14:03 +0800

    Implemented POST to create person with validation

--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { PersonService } from './person.service';
 import { Person } from 'src/entity/person.entity';
+import { ApiCreatedResponse } from '@nestjs/swagger';
 
 @Controller('person')
 export class PersonController {
@@ -16,6 +17,12 @@ export class PersonController {
     findOnePerson(
       @Param('id') id: number): Promise<Person> {
       return this.personsService.findOne(id)
+    }
+
+    @Post()
+    @ApiCreatedResponse({ description: 'The record has been successfully created.'})
+    create(@Body() person: Person) {
+        return this.personsService.create(person)
     }
 
 }
--- src/person/person.service.ts
+++ src/person/person.service.ts
@@ -1,7 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, ForbiddenException } from '@nestjs/common';
 import { Connection } from 'typeorm';
 import { Person } from 'src/entity/person.entity';
 import { HouseholdService } from '../household/household.service'
+import { MaritalStatusType } from 'src/entity/enum-types';
 
 @Injectable()
 export class PersonService {
@@ -10,6 +11,21 @@ export class PersonService {
         private connection: Connection,
         private householdService: HouseholdService
     ) {}
+
+    async validation(person:Person) {
+        const household_unit = await this.householdService.findOne(person.household_unit)
+        if (person.spouse != null) {
+            const spouse = await this.findOne(person.spouse, true)
+            if (person.marital_status == 'Single') {
+                throw new ForbiddenException("Marital status should be set to 'Married' if spouse_id is indicated")
+            }
+        }
+        if (person.spouse == null) {
+            if (person.marital_status == 'Married') {
+                throw new ForbiddenException("Marital status should be set to 'Single' if spouse_id is not indicated")
+            }
+        }
+    }
 
     async getAll(): Promise<Person[]> {
         const manager = this.connection.manager
@@ -26,6 +42,21 @@ export class PersonService {
             }
             throw new NotFoundException('Person does not exist')
         }
+    }
+
+    async create(person: Person) {
+
+        const validation = await this.validation(person)
+
+        await this.connection.transaction(async manager => {
+            const person_ORM = manager.create(Person, person)
+            await manager.save(person_ORM);
+            const personId = manager.getId(person_ORM)
+            if (person_ORM.spouse) {
+                await manager.update(Person, person.spouse, {spouse: personId, marital_status: MaritalStatusType.Married})
+                }
+            return personId
+        })
     }
 
 }
