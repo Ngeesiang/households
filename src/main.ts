b598de3456e1ac8dc0dff51c2e2dd32248c53d52
Ngee Siang <nathan.lingns@gmail.com>
2020-11-08 18:42:47 +0800

    Added SwaggerAPI

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import "reflect-metadata";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+    const options = new DocumentBuilder()
+      .setTitle('Households')
+      .setDescription('Household API description')
+      .setVersion('1.0')
+      .addTag('Household')
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
